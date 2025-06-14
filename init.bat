19e7fce25189276c329208c5d902396431499877
will <WillRowe@gmail.com>
2025-06-14 15:48:44 -0400

    batman

---
+++ init.bat
@@ -0,0 +1,6 @@
+﻿@echo off
+set "repo=https://github.com/DeathRoweX/PowerShell.git"
+set "currentDir=%~dp0"
+git clone %repo% .
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%currentDir%\Scripts\initial_pc_config.ps1"
+pause
